300f17662d18ecac3b8b5c50e7b380255c08702f
LaconicLizard <laconiclizard976@protonmail.com>
2021-05-17 07:50:39 -0700

    Fixed BooleanOption buttons not responding to cycleButtonBack

--- src/main/java/lacliz/refinedui/mixin/OptionButtonWidget_Mixin.java
+++ src/main/java/lacliz/refinedui/mixin/OptionButtonWidget_Mixin.java
@@ -5,10 +5,7 @@ import lacliz.refinedui.Config;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.OptionButtonWidget;
-import net.minecraft.client.options.CyclingOption;
-import net.minecraft.client.options.GameOptions;
-import net.minecraft.client.options.GraphicsMode;
-import net.minecraft.client.options.Option;
+import net.minecraft.client.options.*;
 import net.minecraft.client.resource.VideoWarningManager;
 import net.minecraft.client.util.OrderableTooltip;
 import org.spongepowered.asm.mixin.Final;
@@ -28,35 +25,42 @@ public abstract class OptionButtonWidget_Mixin extends ButtonWidget implements O
         // when pressing a cyclic option button, cycle backwards when the correct mouse button is pressed
         if (Config.get().cycleButtonBack  // check enabled
                 && Config.cycleButtonBack_keyBinding.matchesMouse(button)  // and it's the right button
-                && this.option instanceof CyclingOption) {  // and the option is cyclic
+                && (this.option instanceof CyclingOption || this.option instanceof BooleanOption)) {  // and the option is supported
             // modified from sauce: AbstractButtonWiget.mouseClicked
             if (this.active && this.visible && this.clicked(mouseX, mouseY)) {
-                CyclingOption co = (CyclingOption) this.option;
                 MinecraftClient client = MinecraftClient.getInstance();
                 this.playDownSound(client.getSoundManager());
                 GameOptions options = client.options;
-                // the following special cases have badly implemented .cycle(...) methods, and
-                // require specific overrides to their behavior
-                if (option == Option.GRAPHICS) {
-                    // this block adapted from Option class' static initializer where it sets GRAPHICS
-                    VideoWarningManager videoWarningManager = client.getVideoWarningManager();
-                    GraphicsMode nextMode = GraphicsMode.byId(options.graphicsMode.getId() - 1);
-                    // check whether next graphics option is fabulous, warn if so
-                    if (nextMode == GraphicsMode.FABULOUS && videoWarningManager.canWarn()) {
-                        videoWarningManager.scheduleWarning();
-                    } else {
-                        options.graphicsMode = nextMode;
-                        if (options.graphicsMode == GraphicsMode.FABULOUS && (!GlStateManager.supportsGl30() || videoWarningManager.hasCancelledAfterWarning())) {
-                            options.graphicsMode = GraphicsMode.FAST;
+                if (this.option instanceof CyclingOption) {
+                    CyclingOption co = (CyclingOption) this.option;
+                    // the following special cases have badly implemented .cycle(...) methods, and
+                    // require specific overrides to their behavior
+                    if (option == Option.GRAPHICS) {
+                        // this block adapted from Option class' static initializer where it sets GRAPHICS
+                        VideoWarningManager videoWarningManager = client.getVideoWarningManager();
+                        GraphicsMode nextMode = GraphicsMode.byId(options.graphicsMode.getId() - 1);
+                        // check whether next graphics option is fabulous, warn if so
+                        if (nextMode == GraphicsMode.FABULOUS && videoWarningManager.canWarn()) {
+                            videoWarningManager.scheduleWarning();
+                        } else {
+                            options.graphicsMode = nextMode;
+                            if (options.graphicsMode == GraphicsMode.FABULOUS && (!GlStateManager.supportsGl30() || videoWarningManager.hasCancelledAfterWarning())) {
+                                options.graphicsMode = GraphicsMode.FAST;
+                            }
+                            client.worldRenderer.reload();
                         }
-                        client.worldRenderer.reload();
+                    } else {  // normal case
+                        co.cycle(options, -1);  // cycle back instead of forwards
                     }
-                } else {  // normal case
-                    co.cycle(options, -1);  // cycle back instead of forwards
+                    // taken from CyclingOption.createButton
+                    setMessage(co.getMessage(options));  // update button message
+                    return true;
+                } else if (this.option instanceof BooleanOption) {  // check not necessary, but doing it for organization
+                    BooleanOption bo = (BooleanOption) this.option;
+                    bo.toggle(options);
+                    // from BooleanOption.createButton
+                    setMessage(bo.getDisplayString(options));
                 }
-                // taken from CyclingOption.createButton
-                setMessage(co.getMessage(options));  // update button message
-                return true;
             }
             return false;
         } else {  // act normal
