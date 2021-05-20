ab66e05a72c39df2aaf9d09a3cdebab213c8cb98
LaconicLizard <laconiclizard976@protonmail.com>
2021-05-19 18:53:48 -0700

    Added english translations to clothconfig menu

--- src/main/java/lacliz/refinedui/RUIConfig.java
+++ src/main/java/lacliz/refinedui/RUIConfig.java
@@ -2,18 +2,14 @@ package lacliz.refinedui;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.Tooltip;
 
 import static lacliz.refinedui.RefinedUI.MOD_ID;
 
 @Config(name = MOD_ID)
 public class RUIConfig implements ConfigData {
 
-    @Tooltip
     public boolean textFieldClear = true;
-    @Tooltip
     public boolean cycleButtonBack = true;
-    @Tooltip
     public boolean hotbarCounts = true;
 
 }
